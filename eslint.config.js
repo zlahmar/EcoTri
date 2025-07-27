5acac86b6c7c6fe3783fca9b9cb7a2e11ca48e7a
zlahmar <114932730+zlahmar@users.noreply.github.com>
2025-07-27 15:41:14 +0200

    chore: update dependencies in package.json
    - Added expo-file-system version ~18.1.11
    - Updated devDependencies:
      - Added @eslint/js, @typescript-eslint/eslint-plugin, @typescript-eslint/parser, eslint, and eslint-plugin-react

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+const ts = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const react = require('eslint-plugin-react');
+
+module.exports = [
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      react,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/jsx-uses-react': 'error',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
